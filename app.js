651e806b2e0820fb979e006466f62e43c782bd4d
Vishal Gupta <vishalgupta96644@gmail.com>
2023-07-30 15:30:20 +0530

    Commit Upto Level 5 Security Check

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const port = 3000;
 const app=express();
@@ -13,6 +14,14 @@ const app=express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 app.set("view engine","ejs");
+app.use(session({
+    secret : "Our little secret.",
+    resave : false,
+    saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true});
 
@@ -21,7 +30,13 @@ const userSchema = new mongoose.Schema({
     password : String
 })
 
+userSchema.plugin(passportLocalMongoose)
+
 const User = new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.listen(port,()=>{
     console.log(`Server Started on port ${port}`)
@@ -39,41 +54,50 @@ app.get("/register",(req,res)=>{
     res.render("register");
 })
 
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+      res.render("secrets");
+    } else {
+      res.redirect("/login");
+    }
+  });
+
+app.get("/logout",(req,res,next)=>{
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+})
+
 app.post("/register",async (req,res)=>{
 
-    const hash = bcrypt.hashSync(req.body.password,saltRounds);
-
-    const user = new User({
-        email : req.body.username,
-        password : hash
-    })
-    try{
-        const result = await user.save();
-        if(result)
-            res.render("secrets")
-        else
-            res.send("Login Failed!")
-    }catch(error){
-        console.log(error);
-    }
+    User.register({username : req.body.username},req.body.password ,(err,user)=>{
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req,res,()=>{
+                res.redirect("/secrets")
+            })
+        }
+    }) 
 })
 
 app.post("/login",async (req,res)=>{
-    const username = req.body.username
-    const password = req.body.password
-    try{
-        const result = await User.findOne({email : username})
-        if(result)
-        {
-            if(bcrypt.compareSync(password,result.password))
-                res.render("secrets")
-            else
-                res.send("'Password Does not Match...Try Again !'")
-
+    
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+      });
+     
+      req.login(user, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          passport.authenticate("local")(req, res, function () {
+            res.redirect("/secrets");
+          });
         }
-        else
-            res.send("User Not found...")
-    }catch(error){
-        res.send(error)
-    }
+      });
+
 })
